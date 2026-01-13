21ffcc2ce4c2f6fefdeec9385c6da1a0752b7b94
Gyasi Godwin <sarkcessfulhefy@gmail.com>
2026-01-13 13:17:20 +0000

    updating js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const billAmount = document.getElementById('billAmount');
+const tipButtons = document.querySelectorAll('.tip-btn');
+const customInput = document.querySelector('.custom-input');
+const numPeopleInput = document.getElementById('numPeople');
+const tipPerPerson = document.getElementById('tipPerPerson');
+const totalPerPerson = document.getElementById('totalPerPerson');
+const resetButton = document.querySelector('reset-btn')
